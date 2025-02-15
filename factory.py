a0c36d0b665dce0f2714d09b3f6ec2d3793eeaa1
SergoDobro <optimgrey@yandex.ru>
2025-02-15 20:58:38 +0300

    first iteration of code

---
+++ factory.py
@@ -0,0 +1,79 @@
+class Controller:
+    def __init__(self,map, initial_park):
+        self.vehicles = initial_park
+        self.vehicles_on_rest = initial_park #suppose that all vehicles rest when class is made
+        self.vehicles_on_maintenance = []
+        self.map = map
+
+    def send_car_to_maintainance(self, vehicle):
+        pass
+    def send_car_to_rest(self, vehicle):
+        pass
+    def send_car_to_act(self, vehicle, tunnel):
+        pass
+
+
+
+
+class Vehicle:
+    def __init__(self):
+        super().__init__()
+        self.position_coordinates = (0,0)
+        self.status = "pause"  # pause or moving or acting or maintenance
+        self.current_target_path = None
+
+    def Move(self, path):
+        self.current_target_path = path
+        self.status = "moving"
+        pass
+
+    def Act(self):
+        pass
+
+
+class Vehicle_Drill(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+    def Act(self):
+        #Drill baby Drill!
+        pass;
+
+
+class Vehicle_Mapper(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+    def Act(self):
+        #Я карта!
+        pass;
+
+
+class Vehicle_Checker(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.damaged_tunnel_percentages = {} #Запись инфы о проверенных туннелях, о повреждении креплениий в конкретных туннелях
+
+    def Act(self):
+        #Check tunnel connections!
+        pass;
+
+
+class Path:
+    def __init__(self):
+        self.tunnels = []
+
+
+class Map:
+    def __init__(self, paths):
+        self.paths = paths #list of path lists
+    def BuildPath(self, Tunnel_A, Tunnel_B) -> Path:
+        pass
+    def UpdateTunnelInfo(self, tunnel, connectionType, damage_percentage):
+        pass
+
+class Tunnel:
+    def __init__(self, start_position, end_position, connections):
+        self.start_position = start_position
+        self.end_position = end_position
+        self.connections = connections # {"Arc":0%, "Trapeceidal":5%} Stores connection type data
